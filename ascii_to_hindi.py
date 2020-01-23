04ad0ab933f89324f6541588f7ba2066f5141506
brahmakulkarni <brahma.kulkarni@iiitb.org]>
2020-01-23 09:01:24 +0530

    Added almost complete code for part 1

--- ascii_to_hindi.py
+++ ascii_to_hindi.py
@@ -15,11 +15,11 @@ def convert_to_unicode(word):
     if(len(akshara) == 2):
         if((akshara+'a') in it3_to_utf.keys()):
             unicode += it3_to_utf[(akshara+'a')]
-            unicode += '+'
+            # unicode += +
 
         elif(akshara[0] in it3_to_utf.keys()):
             unicode += it3_to_utf[akshara[0]]
-            unicode += '+\u0948+'
+            unicode += '\u0948'
 
             if((akshara[1:]+'a') in it3_to_utf.keys()):
                 unicode += it3_to_utf[(akshara[1:]+'a')]
@@ -27,7 +27,7 @@ def convert_to_unicode(word):
     if(len(akshara) == 1):
         if((akshara+'a') in it3_to_utf.keys()):
             unicode += it3_to_utf[(akshara+'a')]
-            unicode += '+'
+            # unicode += '+'
 
     #MATRA CHECK
 
@@ -60,7 +60,7 @@ def hinglish_to_hindi(text):
                     word += i[j+1]
                 print(word)
                 unicode += convert_to_unicode(word)
-                unicode += '+'
+                # unicode += '+'
                 word = ""
                 if(global_flag == True):
                     j +=1
@@ -76,62 +76,82 @@ def hinglish_to_hindi(text):
 matras = {
     'aa': '\u093E',
     'i' : '\u093F',
-    'e' : '\u093F',
+    # 'e' : '\u093F',
     'ee': '\u0940',
     'u' : '\u0941',
     'oo': '\u0942',
-    'ae': '\u0947',
+    'e': '\u0947',
     'ai': '\u0948',
     'o' : '\u094B',
     'ow': '\u094C',
 }
 
-it3_to_utf = {
+first = {
     'a': '\u0905',
     'aa': '\u0906',
     'i': '\u0907',
     'ii': '\u0908',
+    'ee': '\u0908',
     'u': '\u0909',
     'uu': '\u090a',
+    'oo': '\u090a',
     'e': '\u090f',
     'ai': '\u0910',
     'o': '\u0913',
     'au': '\u0914',
-    'ka': '\u0915',
-    'kha': '\u0916',
-    'ga': '\u0917',
-    'gha': '\u0918',
-    'cha': '\u091a',
-    'chha': '\u091b',
-    'ja': '\u091c',
-    'jha': '\u091d',
-    'Ta': '\u091f',
-    'Tha': '\u0920',
-    'Da': '\u0921',
-    'Dha': '\u0922',
-    'Na': '\u0923',
-    'ta': '\u0924',
-    'tha': '\u0925',
-    'da': '\u0926',
-    'dha': '\u0927',
-    'na': '\u0928',
-    'pa': '\u092a',
-    'pha': '\u092b',
-    'ba': '\u092c',
-    'bha': '\u092d',
-    'ma': '\u092e',
-    'ya': '\u092f',
-    'ra': '\u0930',
-    'la': '\u0932',
-    'va': '\u0935',
-    'sha': '\u0936',
-    'Sha': '\u0937',
-    'sa': '\u0938',
-    'ha': '\u0939',
-    'pha': '\u095e'
+}
+
+it3_to_utf = {
+    # 'a': '\u0905',
+    # 'aa': '\u0906',
+    # 'i': '\u0907',
+    # 'ii': '\u0908',
+    # 'u': '\u0909',
+    # 'uu': '\u090a',
+    # 'oo': '\u090a',
+    # 'e': '\u090f',
+    # 'ai': '\u0910',
+    # 'o': '\u0913',
+    # 'au': '\u0914',
+    'k': '\u0915',
+    'kh': '\u0916',
+    'g': '\u0917',
+    'gh': '\u0918',
+    'ch': '\u091a',
+    'chh': '\u091b',
+    'j': '\u091c',
+    'jh': '\u091d',
+    'T': '\u091f',
+    'Th': '\u0920',
+    'D': '\u0921',
+    'Dh': '\u0922',
+    'N': '\u0923',
+    't': '\u0924',
+    'th': '\u0925',
+    'd': '\u0926',
+    'dh': '\u0927',
+    'n': '\u0928',
+    'p': '\u092a',
+    'ph': '\u092b',
+    'b': '\u092c',
+    'bh': '\u092d',
+    'm': '\u092e',
+    'y': '\u092f',
+    'r': '\u0930',
+    'l': '\u0932',
+    'v': '\u0935',
+    'sh': '\u0936',
+    'Sh': '\u0937',
+    's': '\u0938',
+    'h': '\u0939',
+    'ph': '\u095e',
+    'ksh': '\u0915'+'\u094d'+'\u0937',
+    'tr': '\u0924'+'\u094d'+'\u0930',
+    'br': '\u092c'+'\u094d'+'\u0930',
+    'z': '\u095b'
 }
 
 
 if __name__ == "__main__":
 
-    hinglish_to_hindi("naanaa")
+    hinglish_to_hindi("kaksha")
--- code1.py
+++ code1.py
@@ -1,44 +1,119 @@
+from nltk.tokenize import word_tokenize
+
 it3_to_utf = {
+    'k': '\u0915',
+    'kh': '\u0916',
+    'g': '\u0917',
+    'gh': '\u0918',
+    'ch': '\u091a',
+    'chh': '\u091b',
+    'j': '\u091c',
+    'jh': '\u091d',
+    'T': '\u091f',
+    'Th': '\u0920',
+    'D': '\u0921',
+    'Dh': '\u0922',
+    'N': '\u0923',
+    't': '\u0924',
+    'th': '\u0925',
+    'd': '\u0926',
+    'dh': '\u0927',
+    'n': '\u0928',
+    'p': '\u092a',
+    'ph': '\u092b',
+    'b': '\u092c',
+    'bh': '\u092d',
+    'm': '\u092e',
+    'y': '\u092f',
+    'r': '\u0930',
+    'l': '\u0932',
+    'v': '\u0935',
+    'sh': '\u0936',
+    'Sh': '\u0937',
+    's': '\u0938',
+    'h': '\u0939',
+    'ph': '\u095e',
+    'ksh': '\u0915'+'\u094d'+'\u0937',
+    'tr': '\u0924'+'\u094d'+'\u0930',
+    'br': '\u092c'+'\u094d'+'\u0930',
+    'z': '\u095b'
+}
+
+first = {
     'a': '\u0905',
     'aa': '\u0906',
     'i': '\u0907',
     'ii': '\u0908',
+    'ee': '\u0908',
     'u': '\u0909',
     'uu': '\u090a',
+    'oo': '\u090a',
     'e': '\u090f',
+    'ai': '\u0910',
     'o': '\u0913',
     'au': '\u0914',
-    'ka': '\u0915',
-    'kha': '\u0916',
-    'ga': '\u0917',
-    'gha': '\u0918',
-    'cha': '\u091a',
-    'chha': '\u091b',
-    'ja': '\u091c',
-    'jha': '\u091d',
-    'Ta': '\u091f',
-    'Tha': '\u0920',
-    'Da': '\u0921',
-    'Dha': '\u0922',
-    'Na': '\u0923',
-    'ta': '\u0924',
-    'tha': '\u0925',
-    'da': '\u0926',
-    'dha': '\u0927',
-    'na': '\u0928',
-    'pa': '\u092a',
-    'pha': '\u092b',
-    'ba': '\u092c',
-    'bha': '\u092d',
-    'ma': '\u092e',
-    'ya': '\u092f',
-    'ra': '\u0930',
-    'la': '\u0932',
-    'va': '\u0935',
-    'sha': '\u0936',
-    'Sha': '\u0937',
-    'sa': '\u0938',
-    'ha': '\u0939',
-    'pha': '\u095e',
-    
 }
+
+matras = {
+    'aa': '\u093E',
+    'i' : '\u093F',
+    # 'e' : '\u093F',
+    'ee': '\u0940',
+    'u' : '\u0941',
+    'oo': '\u0942',
+    'e': '\u0947',
+    'ai': '\u0948',
+    'o' : '\u094B',
+    'ow': '\u094C',
+}
+
+vowels = ['a','i','o','e','u']
+
+def convert_to_unicode(word):
+    unicode = ''
+    i = 0
+    j = 0
+    if (word[:2] in first.keys()):
+        unicode += it3_to_utf[word[:2]]
+        i = 2
+        j = 2
+    elif (word[0] in ['a', 'i', 'u', 'e', 'o']):
+        unicode += it3_to_utf[word[0]]
+        i = 1
+        j = 1
+    else:
+        while (i < len(word)):
+            if (word[i] not in vowels):
+                i+=1
+            else:
+                if (word[i:(i+2)] in matras.keys()):
+                    unicode += it3_to_utf[word[j:i]] + matras[word[i:(i+2)]]  
+                    i += 2
+                    j = i
+                elif (word[i] in matras.keys()):
+                    unicode += it3_to_utf[word[j:i]] + matras[word[i]]
+                    i += 1
+                    j = i
+                elif (word[i] == 'a'):
+                    unicode += it3_to_utf[word[j:i]]
+                    i += 1
+                    j = i
+    return unicode
+
+def tokenize(text):
+    return word_tokenize(text)
+
+def hinglish_to_hindi(text):
+    words = tokenize(text)
+
+    global_flag = False
+    final_sentence_unicode = ''
+    for i in words:
+        unicode = ''
+        unicode += convert_to_unicode(i)
+        final_sentence_unicode += unicode
+        final_sentence_unicode += " "
+    print(final_sentence_unicode)
+
+if __name__ == "__main__":    
+    hinglish_to_hindi("kakshaa")        
